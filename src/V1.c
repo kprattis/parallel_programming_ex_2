ef3df6aea4e0258d49b2c507a025dd2178253149
kprattis <prattiskonstantinos@gmail.com>
2023-01-08 20:34:05 +0200

    some changes

--- src/V1.c
+++ src/V1.c
@@ -61,6 +61,8 @@ knnresult distrAllkNN(double * X, int n, int d, int k){
     int start, end, size;
     int kval;
 
+    //It will take numtasks - 1 interchnges of points before the initial points of every process return.
+    // Add the first iteration of the initial points : total of numtasks calculations
     while(round < numtasks){
         
         MPI_Isend(Y, n * d, MPI_DOUBLE , dest, 1000 + 100 * round + tid , MPI_COMM_WORLD, &mpireq);
@@ -86,8 +88,6 @@ knnresult distrAllkNN(double * X, int n, int d, int k){
                 kselect(D + i * BLOCKSIZE, 0, size - 1, kval, knn.ndist + i * k, knn.nidx + i * k, (b == 0) && (round == 0), start + ((tid + round) % numtasks) * n);
         }
         //--------------------End of own points Calculation----------------------------
-        printf("I am %d at round %d\n", tid, round);
-        print_arrd(Y, n , d);
 
         MPI_Recv(Z, n * d, MPI_DOUBLE , src , 1000 + 100 * round + src , MPI_COMM_WORLD, &mpistat);
 
@@ -108,9 +108,9 @@ knnresult distrAllkNN(double * X, int n, int d, int k){
 
 int main(int argc, char *argv[]){
     char resultsfile[] = "results.txt";
-    char infile[] = "input.txt";
+    //char infile[] = "input.txt";
 
-    FILE *fresults, *f;
+    FILE *f;
     
     MPI_Init(&argc, &argv);
 
@@ -119,19 +119,12 @@ int main(int argc, char *argv[]){
     int tid, numtasks;
     MPI_Comm_rank( MPI_COMM_WORLD , &tid);
     MPI_Comm_size( MPI_COMM_WORLD , &numtasks);
-    printf("%d\n", numtasks);
-    int N = 12, d = 1, k = 2, n;
 
-    int IhaveWritten = 1;
-    int WritingDone = 1;
-
-    int chunk = N / numtasks;
-
+    int N = 12, d = 2, k = 2, n;
+    int chunk = N * d / numtasks;
     int start = tid * chunk;
-    int end = (tid == numtasks - 1) ? N : (tid + 1) * chunk;
-
+    int end = (tid == numtasks - 1) ? N * d : (tid + 1) * chunk;
     n = end - start;
-
     double *X = (double *) malloc(sizeof(double) * n * d);
 
     srand(start * rand());
@@ -139,116 +132,55 @@ int main(int argc, char *argv[]){
         X[i - start] = rand() * 1.0 / RAND_MAX * 10 + -5;
     }
     
-    printf("%d X is:\n", tid);
-    print_arrd(X, n, d);
-    
     struct timeval start_time, end_time;
     double elapsed_time;
-    
-    
-
     gettimeofday(&start_time, NULL);
         knnresult knn = distrAllkNN(X, n, d, k);
     gettimeofday(&end_time, NULL);
     elapsed_time = (end_time.tv_sec - start_time.tv_sec) + (end_time.tv_usec - start_time.tv_usec) / 1000000.0;
-    
-    
-    //print_arri(knn.nidx, m, k);
-    //print_arrd(knn.ndist, m, k);
-    printf("%d Execution Time, %lf\n", tid, elapsed_time);
 
-    printf("%d finished\n", tid);
+    double othertime;
 
-    //print distances
+    //wait for all processes to finish
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    //print results in file. Send all results to the proccess 0.
     if(tid == 0){
-        fresults = fopen(resultsfile, "w");
-        fprint_arrd(fresults, knn.ndist, n, k);
-        fclose(fresults);
-
-        MPI_Send(&IhaveWritten, 1, MPI_INT , tid + 1, tid , MPI_COMM_WORLD);
-
-    }
-    else if(tid == (numtasks - 1)){
-
-        MPI_Recv(&IhaveWritten, 1, MPI_INT , tid - 1, tid - 1 , MPI_COMM_WORLD, &mpistat);
-
-        fresults = fopen(resultsfile, "a");
-        fprint_arrd(fresults, knn.ndist, n, k);
-        fclose(fresults);
         
-        MPI_Send(&IhaveWritten, 1, MPI_INT , 0, 10000 , MPI_COMM_WORLD);
+        for(int i = 1; i < numtasks; i++){
+            MPI_Recv(&othertime, 1, MPI_DOUBLE , i , i * 100 + 3 , MPI_COMM_WORLD, &mpistat);
+            elapsed_time = (elapsed_time < othertime) ? othertime : elapsed_time;
+        }
+        printf("Execution Time, %lf\n", elapsed_time);
+
+
+        f = fopen(resultsfile, "w");
+        
+        fprint_arrd(f, knn.ndist, n, k);
+
+        for(int i = 1; i < numtasks; i++){
+            MPI_Recv(knn.ndist, n * k, MPI_DOUBLE , i , i * 100 + 1 , MPI_COMM_WORLD, &mpistat);
+            fprint_arrd(f, knn.ndist, n, k);
+        }
+
+        fprintf(f, "\n");
+
+        fprint_arri(f, knn.nidx, n, k);
+
+        for(int i = 1; i < numtasks; i++){
+            MPI_Recv(knn.nidx, n * k, MPI_INT , i , i * 100 + 2 , MPI_COMM_WORLD, &mpistat);
+            fprint_arri(f, knn.nidx, n, k);
+        }
+
+
+        fclose(f);
     }
     else{
-        MPI_Recv(&IhaveWritten, 1, MPI_INT , tid - 1, tid - 1 , MPI_COMM_WORLD, &mpistat);
-        printf("I have permission %d\n", tid);
-
-        fresults = fopen(resultsfile, "a");
-        
-        fprint_arrd(fresults, knn.ndist, n, k);
-        fclose(fresults);
-
-        MPI_Send(&WritingDone, 1, MPI_INT , tid + 1, tid , MPI_COMM_WORLD);
+        MPI_Send(&elapsed_time, 1, MPI_DOUBLE , 0 , tid * 100 + 3 , MPI_COMM_WORLD);
+        MPI_Send(knn.ndist, k * n, MPI_DOUBLE , 0 , tid * 100 + 1 , MPI_COMM_WORLD);
+        MPI_Send(knn.nidx, k * n, MPI_INT , 0 , tid * 100 + 2 , MPI_COMM_WORLD);
     }
-
-    //print ids
-    if(tid == 0){
-        MPI_Recv(&WritingDone, 1, MPI_INT , numtasks - 1, 10000 , MPI_COMM_WORLD, &mpistat);
-
-            
-        fresults = fopen(resultsfile, "a");
-        fprintf(fresults, "\n\n");
-        fprint_arri(fresults, knn.nidx, n, k);
-        fclose(fresults);
-
-        MPI_Send(&IhaveWritten, 1, MPI_INT , tid + 1, tid , MPI_COMM_WORLD);
-
-    }
-    else if(tid == (numtasks - 1)){
-        MPI_Recv(&IhaveWritten, 1, MPI_INT , tid - 1, tid - 1 , MPI_COMM_WORLD, &mpistat);
-
-        fresults = fopen(resultsfile, "a");
-        fprint_arri(fresults, knn.nidx, n, k);
-        fclose(fresults);
-
-        
-    }
-    else{
-        MPI_Recv(&IhaveWritten, 1, MPI_INT , tid - 1, tid - 1 , MPI_COMM_WORLD, &mpistat);
-        fresults = fopen(resultsfile, "a");
-        fprint_arri(fresults, knn.nidx, n, k);
-        fclose(fresults);
-        MPI_Send(&IhaveWritten, 1, MPI_INT , tid + 1, tid , MPI_COMM_WORLD);
-    }
-
- if(tid == 0){
-        f = fopen(infile, "w");
-        fprint_arrd(f, X, n, d);
-        fclose(f);
-
-        MPI_Send(&IhaveWritten, 1, MPI_INT , tid + 1, tid , MPI_COMM_WORLD);
-
-    }
-    else if(tid == (numtasks - 1)){
-
-        MPI_Recv(&IhaveWritten, 1, MPI_INT , tid - 1, tid - 1 , MPI_COMM_WORLD, &mpistat);
-
-        f = fopen(infile, "a");
-        fprint_arrd(f, X, n, d);
-        fclose(f);
-        
-        MPI_Send(&IhaveWritten, 1, MPI_INT , 0, 10000 , MPI_COMM_WORLD);
-    }
-    else{
-        MPI_Recv(&IhaveWritten, 1, MPI_INT , tid - 1, tid - 1 , MPI_COMM_WORLD, &mpistat);
-        printf("I have permission %d\n", tid);
-
-        f = fopen(infile, "a");
-        fprint_arrd(f, X, n, d);
-        fclose(f);
-
-        MPI_Send(&WritingDone, 1, MPI_INT , tid + 1, tid , MPI_COMM_WORLD);
-    }
-
+    
     MPI_Finalize();
     return 0;
 }
