0322c76efa42bc2e2734b1b73292ab82c38cb0d2
kprattis <prattiskonstantinos@gmail.com>
2023-01-10 20:31:57 +0200

    ok

--- mpi_batch.sh
+++ mpi_batch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --partition=batch
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-node=3
 #SBATCH --nodes=1
-#SBATCH --time=00:01:00
+#SBATCH --time=00:00:30
 #SBATCH --cpus-per-task=5
 
 srun -n 4 bin/knn_mpi
