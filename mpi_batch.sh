cce32524cbe9f49518a5dbbeb8cf08c10887a080
kprattis <prattiskonstantinos@gmail.com>
2023-01-10 15:44:08 +0200

    ok

--- mpi_batch.sh
+++ mpi_batch.sh
@@ -5,12 +5,4 @@
 #SBATCH --time=0:01:00
 #SBATCH --cpus-per-task=2 
 
-module load gcc/9.4.0 openmpi/4.1.2
-module load openblas
-module load OpenCilk
-
-export OMPI_CC=clang
-
-make mpi CC=clang HPCBLASINC="$OPENBLAS_ROOT/include" LBLAS=$OPENBLAS_ROOT/lib
-
 srun -n 4 bin/knn_mpi
