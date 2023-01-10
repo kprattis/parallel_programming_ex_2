b4eac0e8b1b4cb2f0aa99525627b73474cce8f8b
kprattis <prattiskonstantinos@gmail.com>
2023-01-10 12:46:09 +0200

    okk

--- mpi_batch.sh
+++ mpi_batch.sh
@@ -5,20 +5,4 @@
 #SBATCH --time=0:01:00
 #SBATCH --cpus-per-task=12 
 
-module load gcc/9.4.0 openmpi/4.1.2
-module load openblas
-module load OpenCilk
-
-export OMPI_CC=clang
-
-git clone https://github.com/kprattis/parallel_programming_ex_2
-cd parallel_programming_ex_2
-
-make mpi CC=clang HPCBLASINC="$OPENBLAS_ROOT/include" LBLAS=$OPENBLAS_ROOT/lib/cmake
-
 srun bin/knn_mpi
-
-cd ..
-
-#rm -r parallel_programming_ex_2
-#rmdir parallel_programming_ex_2
