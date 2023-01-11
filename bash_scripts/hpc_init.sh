a5d7581733bf0ce2a26c35d0196e7b9b02205e78
kprattis <prattiskonstantinos@gmail.com>
2023-01-11 16:11:36 +0200

    okk

--- bash_scripts/hpc_init.sh
+++ bash_scripts/hpc_init.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 module load gcc/9.4.0 openmpi/4.1.2
+
+make bin/reg_grid CC=gcc
+
 module load openblas
 module load OpenCilk
 
 export OMPI_CC=clang
 
 make mpi CC=clang HPCBLASINC="$OPENBLAS_ROOT/include" LBLAS=$OPENBLAS_ROOT/lib
-
-make bin/reg_grid CC=gcc
