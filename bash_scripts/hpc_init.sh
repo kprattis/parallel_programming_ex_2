bc1dbf5bbe3306e45bf10d4aeb55c26a4127016c
kprattis <68779380+kprattis@users.noreply.github.com>
2023-01-11 17:06:44 +0200

    Update hpc_init.sh

--- bash_scripts/hpc_init.sh
+++ bash_scripts/hpc_init.sh
@@ -2,6 +2,10 @@
 
 chmod +x bash_scripts/*
 
+module load gcc/9.4.0
+
+make bin/reg_grid CC=gcc
+
 module load gcc/9.4.0 openmpi/4.1.2
 
 module load openblas
@@ -10,4 +14,4 @@ module load OpenCilk
 export OMPI_CC=clang
 make mpi CC=clang HPCBLASINC="$OPENBLAS_ROOT/include" LBLAS=$OPENBLAS_ROOT/lib
 
-#make V0 CC=clang HPCBLASINC="$OPENBLAS_ROOT/include" LBLAS=$OPENBLAS_ROOT/lib
+make V0 CC=clang HPCBLASINC="$OPENBLAS_ROOT/include" LBLAS=$OPENBLAS_ROOT/lib
