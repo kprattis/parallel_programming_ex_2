de5ab917fb0dafc3eb861b2f925c3ac690bbef70
kprattis <prattiskonstantinos@gmail.com>
2023-01-11 23:46:56 +0200

    ok

--- src/test_results.c
+++ src/test_results.c
@@ -2,16 +2,31 @@
 #include <math.h>
 #include <stdlib.h>
 
-int test_result(FILE *fin, FILE *fout, int N, int d){
+int test_result(FILE *fin, FILE *fout){
     int iscorrect = 0;
+    
+    //scan the size and dimension
+    int N, d;
+    fscanf("%d %d\n", &N, &d);
 
+    //stores the points
+    double *p = (double *)malloc(d * sizeof(double));
+
+    int k = pow(3, d);
+
+    double *D = (double *)malloc(k * sizeof(double));
+    int *idx = (int *)malloc(k * sizeof(int));
+
+
+    double *X = (double *)malloc(d * sizeof(double));
+    
     return iscorrect;
 }
 
 int main(int argc, char *argv[]){
 
-    if(argc != 5){
-        printf("Wrong usage. Input the filenames as arguments and then the number of total points and the number of dimensions e.g. : inputs/rg3d1000.txt results/res.txt 1000 3\n");
+    if(argc != 3){
+        printf("Wrong usage. Input the filenames as arguments and then the number of total points and the number of dimensions e.g. : inputs/rg3d1000.txt results/res.txt\n");
         exit(1);
     }
 
@@ -24,10 +39,8 @@ int main(int argc, char *argv[]){
         printf("ERROR: Could not open file %s\n", argv[2]);
         exit(1);
     }
-    int N = atoi(argv[1]);
-    int d = atoi(argv[2]);
 
-    if(test_result(fin, fout, N, d))
+    if(test_result(fin, fout))
         printf("File %s contains the right knn for file %s\n", argv[2], argv[1]);
     else
         printf("File %s does NOT contain the right knn for file %s\n", argv[2], argv[1]);
