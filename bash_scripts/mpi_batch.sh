2a701bc49089f080a24648efedc4eea1d9208e0d
kprattis <prattiskonstantinos@gmail.com>
2023-01-11 16:49:49 +0200

    push

--- bash_scripts/mpi_batch.sh
+++ bash_scripts/mpi_batch.sh
@@ -5,8 +5,13 @@
 #SBATCH --time=00:00:30
 #SBATCH --cpus-per-task=5
 
-module load gcc/9.4.0 openmpi/4.1.2
 module load openblas
 module load OpenCilk
 
+export OMPI_CC=clang
+
+make mpi CC=clang HPCBLASINC="$OPENBLAS_ROOT/include" LBLAS=$OPENBLAS_ROOT/lib
+make V0 CC=clang HPCBLASINC="$OPENBLAS_ROOT/include" LBLAS=$OPENBLAS_ROOT/lib
+
 srun bin/knn_mpi inputs/rg3d500.txt results/V1_rg3d500.txt
+
