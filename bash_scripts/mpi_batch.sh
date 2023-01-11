014cc5803205d7a763d4a178d278e29bdb804b3c
kprattis <prattiskonstantinos@gmail.com>
2023-01-11 16:41:28 +0200

    ok

--- bash_scripts/mpi_batch.sh
+++ bash_scripts/mpi_batch.sh
@@ -5,4 +5,8 @@
 #SBATCH --time=00:00:30
 #SBATCH --cpus-per-task=5
 
+module load gcc/9.4.0 openmpi/4.1.2
+module load openblas
+module load OpenCilk
+
 srun bin/knn_mpi inputs/rg3d500.txt results/V1_rg3d500.txt
